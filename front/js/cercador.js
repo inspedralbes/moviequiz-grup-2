709df7c0c9e832a414a659acb3db1f568a5aff61
a19axechacan <a19axechacan@inspedralbes.cat>
2021-12-02 12:45:00 +0100

    ratings i plot de una pelicula al abir un modal

--- front/js/cercador.js
+++ front/js/cercador.js
@@ -8,6 +8,8 @@ window.onload = function () {
 
 
 
+
+
     let contenedorPelis = document.getElementById("contenedorPelis");
     document.getElementById("buttonSearch").addEventListener("click", function () {
         let parametro = document.getElementById("cercar").value;
@@ -19,6 +21,7 @@ window.onload = function () {
             .then(function (data) {
                 let busqueda = data.Search;
                 let str = "";
+
                 busqueda.forEach(pelicula => {
                     let divPeliStr = `<div class="card col s6 m3">
                     <div class="card-image">
@@ -26,13 +29,52 @@ window.onload = function () {
                       <span class="card-title">${pelicula.Title}</span>
                     </div>
                     <div class="card-action">
-                    <a class="waves-effect waves-light btn modal-trigger"href="#modalAdd"><i class="material-icons right">add</i>Afegir</a>
+                    <a class="waves-effect waves-light btn modal-trigger buttonAfegir" href="#modalAdd" id="${pelicula.imdbID}" ><i class="material-icons right">add</i>Afegir</a>
                     </div>
                   </div>`;
                     str += divPeliStr;
                 });
                 contenedorPelis.innerHTML = str;
+
+
+                contenedorPelis.addEventListener("click", function (e) {
+                    if (e.target.classList.contains("buttonAfegir")) {
+                        let url = `http://www.omdbapi.com/?apikey=cc87f99c&i=${e.target.id}`
+                        fetch(url).then(function (res) {
+                            return res.json()
+                        }).then(function (data) {
+                            let info = data;
+
+                            document.getElementById("modal-title").innerHTML = info.Title;
+                            document.getElementById("modal-image").innerHTML = ` <img class="center-align"src="${info.Poster}" width=250 height=350>`
+                            document.getElementById("modal-plot").innerHTML = info.Plot;
+                            let ulRatings = document.getElementById("modal-ratings");
+                            let str = "";
+                            info.Ratings.forEach(rating => {
+                                let img = "front/img/user-icon.svg"
+                                if (rating.Source === "Rotten Tomatoes") {
+                                    img = "front/img/Rotten_Tomatoes.svg"
+                                } else if (rating.Source === "Metacritic") {
+                                    img = "front/img/Metacritic.svg"
+                                }
+                                let ratingsDiv = ` <li class="collection-item avatar">
+                                <img src=${img} alt="" class="circle">
+                                <span class="title">${rating.Source}</span>
+                                <p>${rating.Value} <br>
+                                   
+                                </p>
+                              </li>`
+                                str += ratingsDiv;
+                            });
+                            ulRatings.innerHTML = str;
+                        })
+                    }
+                })
+
             })
+
+
+
     });
 
 
@@ -43,7 +85,7 @@ window.onload = function () {
     });
 
 
-    document.getElementById("buttonAfegir").addEventListener("click", function () {
+    document.getElementById("buttonConfirmarAfegir").addEventListener("click", function () {
 
         let rating = document.querySelector('input[name="rating"]:checked').value;
 
