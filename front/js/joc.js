b9d2d63ffe4e0f4a7e9f22bbc92f36933d005ebe
a19axechacan <a19axechacan@inspedralbes.cat>
2021-12-13 18:43:27 +0100

    seleccio de la resposta visual

--- front/js/joc.js
+++ front/js/joc.js
@@ -2,15 +2,43 @@
 
 let divPreguntes = document.getElementById("question-container");
 let jocTriggers = document.getElementsByClassName("joc-trigger");
+
 for (let element of jocTriggers) {
     element.addEventListener("click", function () {
-        fetch("http://localhost/pruebas/joc.php").then(function (res) {
+        fetch("http://localhost/pruebas/controller.php").then(function (res) {
 
-            return res.json();
-        }).then(function (data) {
-            let preguntas = data.peliculas;
+            // return res.json();
+        }).then(function (/*data*/) {
+            //  let preguntas = data.peliculas;
             let html = "";
+
+            let preguntas = [
+                {
+                    poster: "https://m.media-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_SX300.jpg",
+                    opcions: [2006, 2002, 2003, 2004],
+                    nom: "Cars"
+                },
+                {
+                    poster: "https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg",
+                    opcions: [2002, 2002, 2003, 2004],
+                    nom: "Spider-man"
+                },
+                {
+                    poster: "https://m.media-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_SX300.jpg",
+                    opcions: [2006, 2002, 2003, 2004],
+                    nom: "Cars"
+                },
+                {
+                    poster: "https://m.media-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_SX300.jpg",
+                    opcions: [2006, 2002, 2003, 2004],
+                    nom: "Cars"
+                }
+
+            ]
+
+
             preguntas.forEach(element => {
+
                 let preguntasHtml = `<div class="pregunta-pelicula">
                 <div class="center-align"><img  src="${element.poster}" width=250 height=350></div>`
                 preguntasHtml += `<div class="preguntes center-align">
@@ -27,18 +55,33 @@ for (let element of jocTriggers) {
             });
             divPreguntes.innerHTML = html;
         }).then(function () {
-            document.getElementById("question-container").addEventListener("click", function (e) {
-                if (e.target.classList.contains("btn-pregunta")) {
-                    console.log("toggle")
-                    e.target.classList.toggle("deep-purple");
-                    e.target.classList.toggle("selected");
-                    if (e.target.classList.contains("selected")) {
-                        console.log("SELECTED!")
-                    } else {
-                        console.log("UNSELECTED!")
-                    }
-                }
-            })
+
         });
     })
 }
+
+
+document.getElementById("question-container").addEventListener("click", function (e) {
+    if (e.target.classList.contains("btn-pregunta")) {
+
+        let divParent = e.target.parentNode.parentNode.parentNode;
+        let buttons = divParent.getElementsByClassName("btn-pregunta");
+
+        for (let button of buttons) {
+            button.classList.remove("deep-purple");
+            button.classList.remove("selected");
+        }
+
+        e.target.classList.toggle("deep-purple");
+        e.target.classList.toggle("selected");
+        if (e.target.classList.contains("selected")) {
+            console.log("SELECTED!")
+        } else {
+            console.log("UNSELECTED!")
+        }
+
+
+
+
+    }
+})
